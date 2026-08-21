3e12e8fd92639bc57bbe0e457a102973745d5fd7
Black Tea <59794561+BlackTeaRemos@users.noreply.github.com>
2026-08-21 12:26:26 +0300

    ci(build): compile mapdir, tier and revision units in all build paths

--- build_msvc.ps1
+++ build_msvc.ps1
@@ -6,7 +6,7 @@ Push-Location $PSScriptRoot
 
 cl /nologo /O2 /LD /EHsc /std:c++latest /D_CRT_SECURE_NO_WARNINGS /I include `
    src\patcher.cpp src\hook.cpp src\redirect.cpp src\overlay.cpp src\overlay_load.cpp `
-   src\manifest.cpp src\edat.cpp src\version_gate.cpp src\version_anchor.cpp src\plugin.cpp src\ipc.cpp src\mapdir.cpp src\tier.cpp `
+   src\manifest.cpp src\edat.cpp src\version_gate.cpp src\version_anchor.cpp src\plugin.cpp src\ipc.cpp src\mapdir.cpp src\tier.cpp src\revision.cpp `
    src\version_shim.cpp /Fe:version.dll /link /DEF:src\version.def /OUT:version.dll
 
 Remove-Item *.obj -ErrorAction SilentlyContinue
